ee0fc1427c93123113d92f93a9216a7fae2bf3b7
EC2 Default User <ec2-user@ip-172-31-13-247.ap-south-1.compute.internal>
2018-10-07 08:06:06 +0000

    deployement Details and configurations

--- application/application/wsgi.py
+++ application/application/wsgi.py
@@ -8,6 +8,14 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+
+path_name = "/home/ec2-user/myspace/mediapp/application"
+if path_name not in sys.path:
+    sys.path.append(path_name)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'application.settings'
 
 from django.core.wsgi import get_wsgi_application
 
